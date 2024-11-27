3fd3d40344391eda53bb0095d6ffe5294aada93a
Gurreman <97109125+Gurreman@users.noreply.github.com>
2024-11-27 16:03:24 +0100

    Add files via upload

--- GitConfig.ps1
+++ GitConfig.ps1
@@ -1,1 +1,3 @@
-Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -CloudDriver Dell,USB -ZTI -Restart
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -ZTI -Restart
+# Removed
+#  -CloudDriver Dell,USB
