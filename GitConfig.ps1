19d3efddf0279dd50041a2556a11d94caa6cff3b
Gurreman <97109125+Gurreman@users.noreply.github.com>
2026-02-23 10:53:15 +0100

    Update GitConfig.ps1

--- GitConfig.ps1
+++ GitConfig.ps1
@@ -1,7 +1,8 @@
-Start-OSDCloudGUI
-#Start-OSDCloud -OSName 'Windows 11 25H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -ZTI -Restart
+#Start-OSDCloudGUI
+Start-OSDCloud -OSName 'Windows 11 25H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -ZTI -Restart
 # Removed
 #  -CloudDriver Dell,USB
+
 
 
 
