1f7138c32d3374e175dfe070a2ba8b7028b81995
Gurreman <97109125+Gurreman@users.noreply.github.com>
2026-03-11 14:12:13 +0100

    Update GitConfig.ps1

--- GitConfig.ps1
+++ GitConfig.ps1
@@ -1,28 +1,7 @@
-#Start-OSDCloud
-add-type @"
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-
-public class TrustAllCertsPolicy : ICertificatePolicy {
-    public bool CheckValidationResult(
-        ServicePoint srvPoint,
-        X509Certificate certificate,
-        WebRequest request,
-        int certificateProblem) {
-        return true;
-    }
-}
-"@
-
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-
-Write-Output "Basically trusting everything now..."
-
-Write-Output "Starting OSDCloud. Good luck."
-
 Start-OSDCloud -OSName 'Windows 11 25H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -SkipODT -ZTI -Restart
 # Removed
 #  -CloudDriver Dell,USB
+
 
 
 
